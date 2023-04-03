3a8534671b501ebee6ea7f5f3cf36b410a805df6
Sensaku <barbe.arnaud@hotmail.fr>
2023-04-03 12:29:13 +0200

    fixed module css

--- viz-kg/src/composant/Navbar.js
+++ viz-kg/src/composant/Navbar.js
@@ -1,14 +1,14 @@
 import React from 'react';
-import "./Navbar.css"
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-    return <section>
-        <div className="logo"><h2>viz-KG</h2></div>
-        <div className="menu">
-            <ul>
-                <li>Menu 1</li>
-                <li>Menu 2</li>
-                <li>Menu 3</li>
+    return <section className={styles['section-menu']}>
+        <div className={styles.logo}><h2>viz-KG</h2></div>
+        <div className={styles.menu}>
+            <ul className={styles["ul-menu"]}>
+                <li className={styles['li-menu']}>Menu 1</li>
+                <li className={styles['li-menu']}>Menu 2</li>
+                <li className={styles['li-menu']}>Menu 3</li>
             </ul>
         </div>
     </section>
