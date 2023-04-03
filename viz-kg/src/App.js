217ee6b2db394ffd1ac1c2f895fd2bc825853f0b
Sensaku <barbe.arnaud@hotmail.fr>
2023-04-03 12:04:18 +0200

    First commit

---
+++ viz-kg/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import TextDisplay from './composant/TextDisplay';
+import Navbar from './composant/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <TextDisplay />
+    </div>
+  );
+}
+
+export default App;
---
+++ viz-kg/src/composant/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Navbar.css"
+
+const Navbar = () => {
+    return <section>
+        <div className="logo"><h2>viz-KG</h2></div>
+        <div className="menu">
+            <ul>
+                <li>Menu 1</li>
+                <li>Menu 2</li>
+                <li>Menu 3</li>
+            </ul>
+        </div>
+    </section>
+    
+}
+
+export default Navbar;
