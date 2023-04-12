dcad1cb1cf789030e03364c74bd978f580e17f2c
Sensaku <barbe.arnaud@hotmail.fr>
2023-04-12 17:07:36 +0200

    Add new graph vizualisation with graph-vis

--- viz-kg/src/pages/GrapheQuery.js
+++ viz-kg/src/pages/GrapheQuery.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useRef} from 'react';
 import styles from "./css/GrapheQuery.module.css";
 import * as d3 from "d3";
+import NetworkChart from '../composant/NetworkChart';
 
 
 const GrapheQuery = () => {
@@ -19,6 +20,7 @@ const GrapheQuery = () => {
     return <div className={styles["content"]}>
         <h1>This is the graphe query vizualisation</h1>
         <svg ref={svgRef} width={svgWidth} height={svgHeight} />
+        <NetworkChart />
     </div>
 }
 
