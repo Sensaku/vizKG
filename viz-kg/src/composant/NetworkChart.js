a262c43a461e4837035eebcaadb9d9efbf591ac1
Sensaku <barbe.arnaud@hotmail.fr>
2023-04-13 11:56:58 +0200

    Refactor network component to be more generic, update css

--- viz-kg/src/composant/NetworkChart.js
+++ viz-kg/src/composant/NetworkChart.js
@@ -1,48 +1,76 @@
 import React, { useState, useEffect, useRef} from 'react';
 import ReactDom from "react-dom";
 import Graph from 'react-graph-vis';
+import styles from "./NetworkChart.module.css";
 
 const NetworkChart = () => {
-    const graph = {
-        nodes: [
-          { id: 1, label: "Node 1", title: "node 1 tootip text" },
-          { id: 2, label: "Node 2", title: "node 2 tootip text" },
-          { id: 3, label: "Node 3", title: "node 3 tootip text" },
-          { id: 4, label: "Node 4", title: "node 4 tootip text" },
-          { id: 5, label: "Node 5", title: "node 5 tootip text" }
-        ],
-        edges: [
-          { from: 1, to: 2 },
-          { from: 1, to: 3 },
-          { from: 2, to: 4 },
-          { from: 2, to: 5 }
-        ]
-      };
-     
-      const options = {
-        layout: {
-          hierarchical: true
-        },
-        edges: {
-          color: "#000000"
-        },
-        height: "500px"
-      };
-     
-      const events = {
-        select: function(event) {
-          var { nodes, edges } = event;
-        }
-      };
+    const [graph, setGraph] = useState({});
+    const [options, setOptions] = useState({});
+    const [events, setEvents] = useState({});
+    const toRender = [];
+  
+    useEffect(() => {
+        const newGraph = {
+            nodes: [
+              { id: 1, label: "Node 1", title: "node 1 tootip text" , color: "red"},
+              { id: 2, label: "Node 2", title: "node 2 tootip text" },
+              { id: 3, label: "Node 3", title: "node 3 tootip text" },
+              { id: 4, label: "Node 4", title: "node 4 tootip text" },
+              { id: 5, label: "Node 5", title: "node 5 tootip text" }
+            ],
+            edges: [
+              { from: 1, to: 2 , label: "test"},
+              { from: 1, to: 3 },
+              { from: 2, to: 4 },
+              { from: 2, to: 5 }
+            ]
+        };
 
-    return <Graph 
-        graph={graph}
-        options={options}
-        events={events}
-        getNetwork={network => {
+        setGraph(newGraph)
 
-        }}
-    />
+        const newOptions = {
+            layout: {
+              hierarchical: false
+            },
+            nodes:{
+                shape: 'circle'
+            },
+            edges: {
+              arrows: 'to',
+              color: "#000000"
+            },
+            height: "500px",
+        };
+
+        setOptions(newOptions)
+
+        const newEvents = {
+            select: function(event) {
+              var { nodes, edges } = event;
+            }
+        };
+
+        setEvents(newEvents)
+
+    },[])
+
+    if(!(Object.keys(graph).length === 0 && graph.constructor === Object)){
+        toRender.push(<Graph 
+            key={1}
+            graph={graph}
+            options={options}
+            events={events}
+            getNetwork={network => {
+    
+            }}
+        />)
+    }
+
+    return <div className={styles["canvas"]}>
+        {toRender}
+    </div>
+
+    
 }
 
 export default NetworkChart;
--- viz-kg/src/pages/GrapheQuery.js
+++ viz-kg/src/pages/GrapheQuery.js
@@ -1,10 +1,39 @@
 import React, { useState, useEffect, useRef} from 'react';
 import styles from "./css/GrapheQuery.module.css";
-import * as d3 from "d3";
 import NetworkChart from '../composant/NetworkChart';
+
+const DisplaySection = () => {
+    const title = "QC1 : Inférence possible"
+    const about = "Visualisation de l'inférence"
+
+    return <section className={styles["query-row"]}>
+        <div className={styles["left-about"]}>
+            <h3>{title}</h3>
+            <p>{about}</p>
+        </div>
+        <div className={styles["right-canvas"]}>
+            <NetworkChart key={1} graph={{}} options={{}} events={{}}/>
+        </div>
+    </section>
+}
 
 
 const GrapheQuery = () => {
+    const sectionList = [<DisplaySection key={"section_query_1"} />, <DisplaySection key={"section_query_2"} />];
+
+    return <div className={styles["content"]}>
+        <h1>This is the graph query vizualisation</h1>  
+        {sectionList}
+    </div>
+}
+
+export default GrapheQuery;
+
+/*
+D3 svg ref
+
+import * as d3 from "d3";
+
     const svgRef = useRef(null);
     const svgWidth = 700;
     const svgHeight = 500;
@@ -16,12 +45,5 @@ const GrapheQuery = () => {
             .attr('height', 100)
             .attr("fill", 'blue')
     },[])
-
-    return <div className={styles["content"]}>
-        <h1>This is the graphe query vizualisation</h1>
-        <svg ref={svgRef} width={svgWidth} height={svgHeight} />
-        <NetworkChart />
-    </div>
-}
-
-export default GrapheQuery;
+//<svg ref={svgRef} width={svgWidth} height={svgHeight} /> 
+*/
